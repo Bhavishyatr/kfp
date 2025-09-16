63831afd122a2c4e04f9333caf00d6c30da45cc6
Bhavishya <52648675+Bhavishyatr@users.noreply.github.com>
2025-09-16 00:51:01 +0000

    create shell components

---
+++ src/pages/AuthorPostsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AuthorPostsPage = () => {
+    return (
+        <div>
+            <h1>Author Posts Page</h1>
+            <p>This is the AuthorPostsPage component.</p>
+        </div>
+    );
+};
+
+export default AuthorPostsPage;
---
+++ src/pages/PostDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostDetailPage = () => {
+    return (
+        <div>
+            <h1>Post Detail Page</h1>
+            <p>This is the post detail page.</p>
+        </div>
+    );
+};
+
+export default PostDetailPage;
